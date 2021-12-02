b0fc2b92dd7f5bf5d58d46cad1c9daf953f908e3
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2021-12-02 11:54:14 +0200

    Do not throw on cron error

--- hooks/hook_cron.php
+++ hooks/hook_cron.php
@@ -4,11 +4,11 @@ function consul_hook_cron(&$croninfo)
 {
     $store = \SimpleSAML\Store::getInstance();
 
-    if (!$store instanceof \SimpleSAML\Module\consul\Store\Consul) {
-        throw new \SimpleSAML\Error\Exception('OAuth2 module: Only Consul Store is supported');
+    if ($store instanceof \SimpleSAML\Module\consul\Store\Consul) {
+        $store->cleanKVStore();
+        $croninfo['summary'][] = 'Cleaned Key-Value Store';
+    } else {
+        // throw new \SimpleSAML\Error\Exception('OAuth2 module: Only Consul Store is supported');
+        $croninfo['summary'][] = 'Store is not of type \SimpleSAML\Module\consul\Store\Consul, skipped';
     }
-
-    $store->cleanKVStore();
-
-    $dbinfo['summary'][] = 'Cleaned Key-Value Store';
 }
